eab59a9397aa70f90e3bbd7cc47fccc2a22797d8
Lewis Allan <bg50xx@student.sunderland.ac.uk>
2017-12-13 16:02:58 +0000

    New menu icon for home added.

--- app/src/main/java/com/example/bg50xx/assignment/Ticket.java
+++ app/src/main/java/com/example/bg50xx/assignment/Ticket.java
@@ -1,8 +1,10 @@
 package com.example.bg50xx.assignment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by bg50xx on 12/12/2017.
@@ -20,5 +22,25 @@ public class Ticket extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent myIntent;
+
+        switch (item.getItemId()){
+            case R.id.action_home:
+                myIntent = new Intent(this.getApplication().getApplicationContext(), MainActivity.class);
+                startActivity(myIntent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                return true;
+//            case R.id.subaction_green:
+//                rl.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
+//                return true;
+//            case R.id.action_user:
+//                createUserDialog();
+//                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
