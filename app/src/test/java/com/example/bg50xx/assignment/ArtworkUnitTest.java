7c98d46c0e31573032e31f745e4f54fa0f4368a1
Lewis Allan <bg50xx@student.sunderland.ac.uk>
2018-01-05 19:17:54 +0000

    All unit tests completed and successful.

--- app/src/test/java/com/example/bg50xx/assignment/ArtworkUnitTest.java
+++ app/src/test/java/com/example/bg50xx/assignment/ArtworkUnitTest.java
@@ -4,6 +4,8 @@ package com.example.bg50xx.assignment;
 import org.junit.Test;
 
 
+import java.text.DecimalFormat;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,7 +14,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ArtworkUnitTest {
-    ClassLoader classLoader = getClass().getClassLoader();
+    DecimalFormat df;
+    Currency cur;
 
     @Test
     public void addArtwork(){
@@ -23,6 +26,50 @@ public class ArtworkUnitTest {
         String actual = art.getTitle();
         String expected = "Test";
         assertEquals("Strings do not match", expected, actual);
+    }
+
+    @Test
+    public void changingArtwork(){
+        String test = "Dummy data";
+        byte[] image = test.getBytes();
+        Artwork art = new Artwork("Test", "Lewis", "Unit Test", 2018, 11, image, 1, 1);
+        art.setTitle("Updated Title");
+
+        String actual = art.getTitle();
+        String expected = "Updated Title";
+        assertEquals("Strings do not match", expected, actual);
+    }
+
+    @Test
+    public void convertGBPtoEur(){
+        df = new DecimalFormat("'€'0.00");
+        double rate = 1.13;
+        double adult = 10.00;
+        String actual = df.format((adult*rate));
+        String expected = "€11.30";
+        assertEquals("Wrong conversion",expected,actual);
+
+    }
+
+    @Test
+    public void discountStudent(){
+        df = new DecimalFormat("'£'0.00");
+        double discount = 0.3;
+        double adult = 10.00;
+        String actual = df.format((adult*(1-discount)));
+        String expected = "£7.00";
+        assertEquals("Wrong conversion",expected,actual);
+
+    }
+
+    @Test
+    public void getRate(){
+        cur = new Currency();
+        cur.setRate(1.25f);
+        float actual = cur.getRate();
+        float expected = 1.25f;
+        assertEquals("Wrong exchange rate",expected,actual,0.1);
+
     }
 
 }
